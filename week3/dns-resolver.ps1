7481d2bcea3eae4272b6a984ed5d93a9f732a65e
AidanP017 <112411033+AidanP017@users.noreply.github.com>
2026-01-31 15:51:18 -0500

    Added dns-resolver.ps1

---
+++ week3/dns-resolver.ps1
@@ -0,0 +1,23 @@
+﻿param(
+    [Parameter(Mandatory=$true)]
+    [string]$prefix,
+
+    [Parameter(Mandatory=$true)]
+    [string]$dns
+)
+
+for ($i = 1; $i -le 254; $i++) {
+    $ip = "$prefix.$i"
+    try {
+        $results = Resolve-DnsName -DnsOnly $ip -Server $dns -ErrorAction Ignore
+
+        $ptr = $results | Where-Object {$_.QueryType -eq "PTR"}
+
+        if ($ptr) {
+            "{0} {1}" -f $ip, $ptr.NameHost
+        }
+
+    } catch {
+        continue
+    }
+}
